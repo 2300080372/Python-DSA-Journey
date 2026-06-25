c561efa488f2489926b88c5ee1814bff9311aca1
Amrutha Thulluru <163846472+2300080372@users.noreply.github.com>
2026-06-25 16:17:46 +0530

    Added Day 1 - Variables and Data Types
    Uploaded Python Day 1 practice programs.

---
+++ Python/Day01_Variables.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+name = "Amrutha"
+age = 20
+cgpa = 8.01
+is_student = True
+
+print(name)
+print(age)
+print(cgpa)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
